0c2f51cb89b30351b78d13d3294bb4ed59ce4f7a
Krystie-W <tsukio.scoring@gmail.com>
2021-11-09 14:03:20 +1100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form'); 
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('#searchByID');
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultsSummary = document.querySelector('section#movieDetails p')
+            const resultsTitle = document.querySelector('section#movieDetails h4')
+                resultsTitle.innerHTML = data.title;
+                resultsSummary.innerHTML = data.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
